884ca7a217d56c474caee5689007c765d5b9f609
Hyerin Lee <hrlee113@gmail.com>
2021-07-14 21:43:02 +0900

    Update user_prep.py

--- prep/user_prep.py
+++ prep/user_prep.py
@@ -2,6 +2,14 @@ import os
 import numpy as np
 import pandas as pd
 
+'''
+Load
+'''
+
 def userloader(filename='steam_user_meta_data_final_v1.csv'):
     user = pd.read_csv(os.path.join('data', filename))
     return user
+
+
+
+
